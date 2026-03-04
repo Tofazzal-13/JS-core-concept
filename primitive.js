d617a4687611190e97c6c2976c3ef969da7101bf
Tofazzal-13 <dev.tofazzal13@gmail.com>
2026-03-04 16:09:03 +0600

    primitive and non primitive

---
+++ primitive.js
@@ -0,0 +1,20 @@
+let data = 4;
+data = "tormuj";
+data = false;
+data = {}
+data = []
+console.log(typeof data);
+
+
+// primitive data type
+let number = 1;
+let string = "string";
+let bollean = false;
+
+// nod primitive data type 
+
+let array = [];
+let object = {};
+function(){
+    
+}
